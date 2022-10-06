eae47e9b8c5922467fd90724be4aa3a28f6a8af6
Ryan Cory <rynocory@comcast.net>
2022-10-06 19:58:45 -0400

    File names added

--- lib/content_searcher.rb
+++ lib/content_searcher.rb
@@ -1,2 +1,22 @@
 class ContentSearcher
+
+def sort_by_name_alphabetical
+
+end
+
+def sort_by_name_anti_alphibetical
+
+end
+
+def sort_by_location
+
+end
+
+def sort_by_date_posted
+
+end
+
+
+
+
 end
