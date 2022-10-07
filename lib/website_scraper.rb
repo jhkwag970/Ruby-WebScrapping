7eb883715783362aace4b4013a8459e93f38f726
jhkwag970 <jhkwag970@gmail.com>
2022-10-06 20:56:58 -0400

    web_scrapping

--- lib/website_scraper.rb
+++ lib/website_scraper.rb
@@ -1,13 +1,34 @@
 require_relative './site_parser'
 require_relative './content_searcher'
+require "mechanize"
 
+agent = Mechanize.new
+page = agent.get "https://osu.wd1.myworkdayjobs.com/OSUCareers"
+page2 = agent.get "https://osu.edu"
+
+puts page.title
+puts page2.title
+puts page.uri
+
+
+=begin
 class WebsiteScraper
 
-    def initialize()
+    def initialize
         @site_parser = SiteParser.new
         @content_searcher = ContentSearcher.new
     end
 
-    def start()
+    def start
+        doc = Nokogiri::HTML4(URI.open("https://osu.wd1.myworkdayjobs.com/OSUCareers"))
+
+        description = doc.css("div").text
     end
+
 end
+=end
+
+
+
+
+
