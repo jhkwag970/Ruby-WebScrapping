77451a1f4585bdabf3517f453ff5cf77017b35ad
jhkwag970 <jhkwag970@gmail.com>
2022-10-06 20:56:58 -0400

    website_scraper

--- lib/website_scraper.rb
+++ lib/website_scraper.rb
@@ -1,13 +1,25 @@
 require_relative './site_parser'
 require_relative './content_searcher'
+require 'open-uri'
+require 'net/http'
+require "nokogiri"
 
 class WebsiteScraper
 
-    def initialize()
+    def initialize
         @site_parser = SiteParser.new
         @content_searcher = ContentSearcher.new
     end
 
-    def start()
+    def start
+        doc = Nokogiri::HTML4(URI.open("https://osu.wd1.myworkdayjobs.com/OSUCareers"))
+
+        description = doc.css("div").text
     end
+
 end
+
+
+
+
+
