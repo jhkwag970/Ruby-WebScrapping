163c9caf0b38fac5628d13489e32eedc5dc7f78d
Ryan Cory <rynocory@comcast.net>
2022-10-07 13:56:02 -0400

    Implemented content_searcher

--- lib/content_searcher.rb
+++ lib/content_searcher.rb
@@ -1,22 +1,27 @@
 class ContentSearcher
 
-def sort_by_name_alphabetical
-
+def sort_by_name_alphabetical(listOfJobs)
+    listOfJobs[:job].sort
 end
 
-def sort_by_name_anti_alphibetical
-
+def sort_by_name_anti_alphibetical(listOfJobsGiven)
+    listOfJobsGiven[:job].sort{|a, b| b <=> a}
 end
 
-def sort_by_location
-
+def sort_by_location(listOfLocations, locations)
+    acceptableLocations = []
+    acceptableLocationsCounter = 0
+    listOfLocations.each do |locGiven|
+        if locGiven[:location] == locations
+            acceptableLocations[acceptableLocationsCounter] = locGiven
+            acceptableLocationsCounter = acceptableLocationsCounter + 1
+        end
+    end
+    listOfLocations
 end
 
-def sort_by_date_posted
-
+def sort_by_date_posted(listOfDates)
+    listOfDates.sort_by[:date]{|d| m,d,y=d.split("/");[y,m,d]}
 end
-
-
-
 
 end
