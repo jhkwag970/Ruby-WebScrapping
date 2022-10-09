af880012044becee00d875491a18aec8f01bdf8c
kavlekar101 <kavlekar101@gmail.com>
2022-10-09 13:18:40 -0400

    finished the output

--- lib/content_searcher.rb
+++ lib/content_searcher.rb
@@ -18,25 +18,6 @@ def find_title(parsed_data, title)
     end
     article
 end
-def sort_by_name_alphabetical(listOfJobs)
-    listOfJobs[:job].sort
-end
-
-def sort_by_name_anti_alphabetical(listOfJobsGiven)
-    listOfJobsGiven[:job].sort{|a, b| b <=> a}
-end
-
-def sort_by_location(listOfLocations, locations)
-    acceptableLocations = []
-    acceptableLocationsCounter = 0
-    listOfLocations.each do |locGiven|
-        if locGiven[:location] == locations
-            acceptableLocations[acceptableLocationsCounter] = locGiven
-            acceptableLocationsCounter = acceptableLocationsCounter + 1
-        end
-    end
-    listOfLocations
-end
 
 def sort_help(h)
     hash = { January: "01", February: "02", March: "03", April: "04", May: "05", June: "06",
@@ -47,7 +28,6 @@ end
 
 def sort_by_date_posted(listOfDates)
     listOfDates.sort_by {|h| sort_help(h)}
-    #listOfDates.map { |hash|  {date: hash[:date], title: hash[:title], link: hash[:link]}}
 end
 
 def sort_by_link(listOfLinks)
--- main.rb
+++ main.rb
@@ -4,4 +4,22 @@ ws = WebsiteScraper.new()
 
 ws.start()
 
-puts ws.content_searcher.sort_by_date_posted ws.site_parser.parsed_data
+# sorts: title, rev title, names alpha, names reverse alpha, location, date, link
+
+puts "Please enter the way you would like the articles to be sorted.
+      \n1.) Enter \"title\" for sorting each entry by title in alphabetical order
+      \n2.) Enter \"rev title\" for sorting each entry by title in reverse alphabetical order
+      \n3.) Enter \"date\" for sorting each entry by date
+      \n4.) Enter \"link\" for sorting each entry by link:\n"
+
+answer = gets.chomp
+
+if answer == "title"
+  puts ws.content_searcher.sort_by_title ws.site_parser.parsed_data
+elsif answer == "rev title"
+  puts ws.content_searcher.sort_by_title_reverse ws.site_parser.parsed_data
+elsif answer == "date"
+  puts ws.content_searcher.sort_by_date_posted ws.site_parser.parsed_data
+elsif answer == "link"
+  puts ws.content_searcher.sort_by_link ws.site_parser.parsed_data
+end
