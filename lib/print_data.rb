0228d2a3725bc8ed95a1537cf575f31fa0177bed
jhkwag970 <jhkwag970@gmail.com>
2022-10-08 21:23:00 -0400

    parsing link added

--- lib/print_data.rb
+++ lib/print_data.rb
@@ -1,9 +1,14 @@
 require_relative './website_scraper'
 
 ws = WebsiteScraper.new
+sp = SiteParser.new
 
 news = ws.start
+sp.parse_news_html(news);
+sp.print_all
+p sp.news_number
 
+=begin
 date = news[0].search('.pp_date_day').text.strip
 month = news[0].search('.pp_date_month').text.strip
 year = news[0].search('.pp_date_year').text.strip
@@ -12,19 +17,10 @@ puts "#{year}-#{month}-#{date}"
 
 header = news[0].search('.text_latestnews_more')
 title = header.text
+link = header[0]['href']
 
 puts "title: #{title}"
-=begin
-header is
-<a class="text_latestnews_more" href="//news.osu.edu/computer-science-benefits-students-with-learning-disabilities/">
-  Computer science benefits students with learning disabilities
-</a>
-but cannot find how to extract href value
-
-I tried but not work
-link = header.href
-link = hdear.attribute['href']
-puts "#{link}"
+puts link
 =end
 
 
--- lib/site_parser.rb
+++ lib/site_parser.rb
@@ -8,11 +8,22 @@ class SiteParser
         news.each do |article|
             article_date = article.search('.pp_date_month').text.strip + "/" + article.search('.pp_date_day').text.strip + "/" + article.search('.pp_date_year').text.strip
             article_title = article.search('.text_latestnews_more').text
-            @parsed_data.append(article_hash(article_date, article_title))
+            article_link = article.search('.text_latestnews_more')[0]['href']
+            @parsed_data.append(article_hash(article_date, article_title,article_link))
         end
     end
 
-    def article_hash(article_date, article_title)
-        {date: article_date, title: article_title}
+    def article_hash(article_date, article_title, article_link)
+        {date: article_date, title: article_title, link: article_link}
     end
+
+    def print_all
+        p @parsed_data
+    end
+
+    def news_number
+        @parsed_data.length
+    end
+
+
 end
